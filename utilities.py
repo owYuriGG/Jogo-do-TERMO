63d2a3aa181e7e7ef8e948dc4c278aadb5769a28
Yuri Garcia <yurigamergarcia@gmail.com>
2023-11-20 20:36:04 -0300

    Adicionada verificação de repetição de palavras

--- utilities.py
+++ utilities.py
@@ -10,7 +10,7 @@ def clear():
 
 
 def verify_word(word):
-    used_words_file = open('used_words.txt', 'r')
+    used_words_file = open(r'Python\Lab02\Semana12\TrabalhoG2\used_words.txt', 'r')
     if word in used_words_file:
         word = sort_word()
         used_words_file.close()
@@ -19,7 +19,7 @@ def verify_word(word):
 
 
 def sort_word():
-    words_file = open('words.txt', 'r')
+    words_file = open('Python\Lab02\Semana12\TrabalhoG2\words.txt', 'r')
     number = random.randint(0, 100)
     for _ in range(number):
         word = words_file.readline().rstrip('\n')
@@ -30,23 +30,24 @@ def sort_word():
 
     return word.upper()
 
-def user_input():
+def user_input(used_words):
     try:
-        input_word = str(input("Digite uma palavra: "))
+        input_word = str(input("Digite uma palavra: ")).upper()
 
         assert len(input_word) == 5, 'A palavra digitada deve ter 5 letras! '
         assert verify_digits(input_word) == False, 'Você não pode digitar números!'
         assert verify_space(input_word) == False, 'Você não pode deixar espaços em branco!'
+        assert input_word not in used_words, 'Você não pode usar a mesma palavra mais de uma vez em um jogo!'
 
         return input_word.upper()
     
     except AssertionError as error:
         print(f'[ERRO] {error}')
-        input_word = user_input()
+        input_word = user_input(used_words)
         return input_word
     except BaseException as error:
         print(f'[ERRO] Um erro ocorreu: {error}')
-        input_word = user_input()
+        input_word = user_input(used_words)
         return input_word
 
 
